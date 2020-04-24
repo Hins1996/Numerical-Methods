5d09f5eef99eef2a0591a05226d6019f22e840d6
Zhaoxuan Hins Hu <55725977+Hins1996@users.noreply.github.com>
2020-04-23 20:25:11 -0500

    Update 01 - 2d-CT+Richardson.py
    Two-dimensional integration by composite trapezoid method with Richardson extrapolation

--- 01 - 2d-CT+Richardson.py
+++ 01 - 2d-CT+Richardson.py
@@ -1,33 +1,3 @@
-'''##### PART 1 #####
-Example functions
-1. f1 and f2 are common functions to demonstrate the CT method
-2. f3 and f4 are used to demonstrate measure transformation techniques in
-   numerical integration
-'''
-
-def f1(x, y):
-    # function f = e^(x+y)
-    return np.exp(x+y)
-
-
-def f2(x, y):
-    # function f = e^(x*y)
-    return np.exp(x*y)
-
-
-def f3(r, theta):
-    # function f = e^(x*y) * r, where x = r*cos(theta) and y = r*sin(theta)
-    x = r*np.cos(theta)
-    y = r*np.sin(theta)
-    return np.exp(x*y)*r
-
-
-def f4(r, theta):
-    # function f = (x+y) * r, where x = r*cos(theta) and y = r*sin(theta)
-    x = r*np.cos(theta)
-    y = r*np.sin(theta)
-    return (x+y)*r
-
 
 '''##### PART 2 #####
 Main function to do integration
@@ -90,3 +60,34 @@ def integral(func, exact_sol, num_interval, xrange, yrange):
         return [Qcr_list, Qcr_Rd_list]
     else:
         return [error, error_Rd]
+
+
+'''##### PART 2 #####
+Example functions
+1. f1 and f2 are common functions for demonstrate
+2. f3 and f4 are used to demonstrate measure transformation techniques in
+   numerical integration
+'''
+
+def f1(x, y):
+    # function f = e^(x+y)
+    return np.exp(x+y)
+
+
+def f2(x, y):
+    # function f = e^(x*y)
+    return np.exp(x*y)
+
+
+def f3(r, theta):
+    # function f = e^(x*y) * r, where x = r*cos(theta) and y = r*sin(theta)
+    x = r*np.cos(theta)
+    y = r*np.sin(theta)
+    return np.exp(x*y)*r
+
+
+def f4(r, theta):
+    # function f = (x+y) * r, where x = r*cos(theta) and y = r*sin(theta)
+    x = r*np.cos(theta)
+    y = r*np.sin(theta)
+    return (x+y)*r
